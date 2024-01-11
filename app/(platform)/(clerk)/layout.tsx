84b68c42115280a8eaeed62478384edec5791be9
Rohit Agrawal <rohit777agrawal@gmail.com>
2024-01-11 15:47:05 -0600

    Authentication Middleware Created (Sign-in and Sign-up)

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,14 @@
+const ClearkLayout = ({
+    children
+}:
+{
+    children: React.ReactNode;
+}) =>{
+    return(
+        <div className="h-full flex items-center justify-center">
+            {children}
+        </div>
+    )
+}
+
+export default ClearkLayout;

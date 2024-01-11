0466c1c10558a6b5b72335efec02715d607681c3
Rohit Agrawal <rohit777agrawal@gmail.com>
2024-01-11 12:54:19 -0600

    marketing page created

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,57 @@
+import { Button } from "@/components/ui/button";
+import localFont from 'next/font/local';
+import { Poppins } from "next/font/google"; 
+import {Medal} from "lucide-react"; //This is a picture
+import Link from "next/link";
+import { cn } from "@/lib/utils"; //Helps to append new items to anything
+
+//How to use Custom Fonts
+const headingFont = localFont({
+    src: "../../public/fonts/font.woff2"
+});
+
+//How to use Google Fonts
+const textFont = Poppins({
+    subsets: ["latin"],
+    weight: [
+        "100",
+        "200",
+        "300",
+        "400",
+        "500",
+        "600",
+        "700",
+        "800",
+        "900"
+    ]
+});
+
+const MarketingPage = () =>{
+    return(
+        <div className="flex items-center justify-center flex-col">
+            <div className={cn("flex items-center justify-center flex-col", headingFont.className)}>
+                <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
+                   <Medal className="h-6 w-6 mr-2"/>
+                   No 1 task management
+                </div>
+                <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
+                    FlagOH helps team move
+                </h1>
+                <div className="text-3xl md:text-6xl bg-gradient-to-t from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit ">
+                    work forward.
+                </div>
+            </div>
+            <div className={cn("text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto", textFont.className)}>
+                Colaborate, manage projects, and reach new productivity peaks. From high rises to the hone office,
+                the way your team workds is unique - accomplish it all with FlagOH.
+            </div>
+            <Button className="mt-6" size="lg" asChild>
+                <Link href="/sign-up">
+                    Get FlagOH for free
+                </Link>
+            </Button>
+        </div>
+    );
+};
+
+export default MarketingPage;

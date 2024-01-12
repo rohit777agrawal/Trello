61503312b55c93a73c55ada3b828e95fcc5c579b
Rohit Agrawal <rohit777agrawal@gmail.com>
2024-01-12 17:29:50 -0600

    Learned Something

--- actions/create-board.ts
+++ actions/create-board.ts
@@ -1,25 +1,53 @@
 "use server";
+import {z} from "zod";
 import { db } from "@/lib/db";
 import { revalidatePath } from "next/cache";
-import {z} from "zod";
+import { redirect } from "next/navigation";
+
+
+export type State = {
+    errors?: {
+        title?: string[];
+    },
+    message?: string | null;
+}
 
 const CreateBoard = z.object({
-    title: z.string(),
+    title: z.string().min(3, {
+        message: "Minimum length of 3 letters is required"
+    })
 });
 
-export async function create(formData: FormData){
+export async function create(prevState:State, formData: FormData){
 
     // const title = formData.get("title") as string;
     //instead of uppercode use this to create title to check they are string
-    const {title} = CreateBoard.parse({
+    const validatedFields = CreateBoard.safeParse({
         title: formData.get("title"),
-    })
+    });
 
-    await db.board.create({
-        data:{
-            title,
+    if(!validatedFields.success){
+        return{
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: "Missing fields."
         }
-    })
+    }
+
+    const {title} = validatedFields.data;
+
+    try{
+        await db.board.create({
+            data:{
+                title,
+            }
+        })
+    } catch(error){
+        return{
+            message: "Database Error",
+        }
+    }
+    
 
     revalidatePath("/organization/org_2apL7IeU41p9SDH7jDrQpuTJcx4");
+    redirect("/organization/org_2apL7IeU41p9SDH7jDrQpuTJcx4");
 }

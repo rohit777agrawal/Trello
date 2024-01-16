38f01a68fcf0ba433aecc468a562bf724aef5ea4
Rohit Agrawal <rohit777agrawal@gmail.com>
2024-01-16 01:07:51 -0600

    Audit log added to all actions

--- actions/delete-board/index.ts
+++ actions/delete-board/index.ts
@@ -9,8 +9,8 @@ import { createSafeAction } from "@/lib/create-safe-action";
 import { DeleteBoard } from "./schema";
 import { InputType, ReturnType } from "./types";
 import { redirect } from "next/navigation";
-// import { createAuditLog } from "@/lib/create-audit-log";
-// import { ACTION, ENTITY_TYPE } from "@prisma/client";
+import { createAuditLog } from "@/lib/create-audit-log";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -32,12 +32,12 @@ const handler = async (data: InputType): Promise<ReturnType> => {
       },
     });
 
-    // await createAuditLog({
-    //   entityTitle: board.title,
-    //   entityId: board.id,
-    //   entityType: ENTITY_TYPE.BOARD,
-    //   action: ACTION.UPDATE,
-    // })
+    await createAuditLog({
+      entityTitle: board.title,
+      entityId: board.id,
+      entityType: ENTITY_TYPE.BOARD,
+      action: ACTION.UPDATE,
+    })
   } catch (error) {
     return {
       error: "Failed to Delete."
--- actions/delete-list/index.ts
+++ actions/delete-list/index.ts
@@ -8,8 +8,8 @@ import { createSafeAction } from "@/lib/create-safe-action";
 
 import { DeleteList } from "./schema";
 import { InputType, ReturnType } from "./types";
-// import { createAuditLog } from "@/lib/create-audit-log";
-// import { ACTION, ENTITY_TYPE } from "@prisma/client";
+import { createAuditLog } from "@/lib/create-audit-log";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -20,7 +20,7 @@ const handler = async (data: InputType): Promise<ReturnType> => {
     };
   }
 
-  const { id, boardId} = data;
+  const { id, boardId } = data;
   let list;
 
   try {
@@ -34,20 +34,20 @@ const handler = async (data: InputType): Promise<ReturnType> => {
       },
     });
 
-    // await createAuditLog({
-    //   entityTitle: board.title,
-    //   entityId: board.id,
-    //   entityType: ENTITY_TYPE.BOARD,
-    //   action: ACTION.UPDATE,
-    // })
+    await createAuditLog({
+      entityTitle: list.title,
+      entityId: list.id,
+      entityType: ENTITY_TYPE.LIST,
+      action: ACTION.DELETE,
+    })
   } catch (error) {
     return {
-      error: "Failed to Delete."
+      error: "Failed to delete."
     }
   }
 
   revalidatePath(`/board/${boardId}`);
-  return {data : list};
+  return { data: list };
 };
 
 export const deleteList = createSafeAction(DeleteList, handler);

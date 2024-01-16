730f7327b765c10f61aed1646a628b4fd3f8450f
Rohit Agrawal <rohit777agrawal@gmail.com>
2024-01-15 18:36:16 -0600

    Drag and Drop functionality created

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Card } from "@prisma/client";
-// import { Draggable } from "@hello-pangea/dnd";
+import { Draggable } from "@hello-pangea/dnd";
 
 // import { useCardModal } from "@/hooks/use-card-modal";
 
@@ -17,19 +17,19 @@ export const CardItem = ({
 //   const cardModal = useCardModal();
 
   return (
-    // <Draggable draggableId={data.id} index={index}>
-    //   {(provided) => (
+    <Draggable draggableId={data.id} index={index}>
+      {(provided) => (
         <div
-        //   {...provided.draggableProps}
-        //   {...provided.dragHandleProps}
-        //   ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
           role="button"
         //   onClick={() => cardModal.onOpen(data.id)}
           className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
         >
           {data.title}
         </div>
-    //   )}
-    // </Draggable>
+      )}
+    </Draggable>
   );
 };
